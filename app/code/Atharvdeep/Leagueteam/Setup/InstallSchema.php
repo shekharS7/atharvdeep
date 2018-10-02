c6abd2c7f4ba3b9957e22d2c772b9cbfba4cb013
Shekhar Suman <sekhars@seedolabs.com>
2018-10-02 18:14:27 +0530

    Registration with bank details and address

---
+++ app/code/Atharvdeep/Leagueteam/Setup/InstallSchema.php
@@ -0,0 +1,149 @@
+<?php
+/**
+
+ * Atharvdeep Leagueteam
+ *
+ * @category Atharvdeep
+ * @package Atharvdeep_Leagueteam
+ * @author Shekhar Team shekharsuman97@gmail.com
+ *
+ */
+/**
+ * Copyright © 2018 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Atharvdeep\Leagueteam\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+
+        $installer->startSetup();
+
+        /**
+         * Create table 'atharvdeep_legteam'
+         */
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('atharvdeep_leagueteam')
+        )->addColumn(
+            'pk',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+            'Primary key'
+        )->addColumn(
+            'member_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            12,
+            ['nullable' => false, 'default' => '0'],
+            'Member Id'
+        )->addColumn(
+            'parent_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            12,
+            ['nullable' => false, 'default' => '0'],
+            'Parent Id'
+        )->addColumn(
+            'level1',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            12,
+            ['nullable' => false, 'default' => ''],
+            'Level 1'
+        )->addColumn(
+            'level2',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => false, 'default' => ''],
+            'Level 2'
+        )->addColumn(
+            'level3',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false, 'default' => ''],
+            'Level 1'
+        )->addColumn(
+            'level4',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false, 'default' => ''],
+            'Level 4'
+        )->addColumn(
+            'level5',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false, 'default' => ''],
+            'Level 5'
+        )->addColumn(
+            'level6',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false, 'default' => ''],
+            'Level 6'
+        )->addColumn(
+            'level7',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false, 'default' => ''],
+            'Level 7'
+        )->addColumn(
+            'level8',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false, 'default' => ''],
+            'Level 8'
+        )->addColumn(
+            'level9',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false, 'default' => ''],
+            'Level 9'
+        )->addColumn(
+            'level10',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false, 'default' => ''],
+            'Level 10'
+        )->addColumn(
+            'created_at',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+            'Created At'
+        )->addColumn(
+            'updated_at',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+            'Updated At'
+        )->addColumn(
+            'path',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            null,
+            ['nullable' => false, 'default' => ''],
+            'Tree Path'
+        )->addIndex(
+            $installer->getIdxName('atharvdeep_leagueteam', ['member_id']),
+            ['member_id'],
+            ['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
+        )->setComment(
+            'Atharvdeep Leagueteam'
+        );
+        $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
+    }
+}
