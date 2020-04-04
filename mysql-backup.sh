2ae482838cc45f9c6ea9fac8f47b70bf0b627a24
root <root@instance-1.c.e-commerce-demo-203212.internal>
2020-04-04 11:20:21 +0000

    dbbackup push to git

---
+++ mysql-backup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+################################################################
+##
+##   MySQL Database Backup Script 
+##   Written By: Shekhar Suman
+##   Last Update: Feb 11, 2019
+##
+################################################################
+
+export PATH=/bin:/usr/bin:/usr/local/bin
+TODAY=`date +"%d%b%Y"`
+
+################################################################
+################## Update below values  ########################
+
+DB_BACKUP_PATH='/home/seedo/dbbackup'
+MYSQL_HOST='localhost'
+MYSQL_PORT='3306'
+MYSQL_USER='root'
+MYSQL_PASSWORD='Ymg#IGS9'
+DATABASE_NAME='atharvdeep'
+BACKUP_RETAIN_DAYS=5   ## Number of days to keep local backup copy
+
+#################################################################
+
+mkdir -p ${DB_BACKUP_PATH}/${TODAY}
+echo "Backup started for database - ${DATABASE_NAME}"
+
+
+mysqldump -h ${MYSQL_HOST} \
+		  -P ${MYSQL_PORT} \
+		  -u ${MYSQL_USER} \
+		  -p${MYSQL_PASSWORD} \
+		  ${DATABASE_NAME} | gzip > ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}-${TODAY}.sql.gz
+
+if [ $? -eq 0 ]; then
+  echo "Database backup successfully completed"
+else
+  echo "Error found during backup"
+fi
+
+
+##### Remove backups older than {BACKUP_RETAIN_DAYS} days  #####
+
+DBDELDATE=`date +"%d%b%Y" --date="${BACKUP_RETAIN_DAYS} days ago"`
+
+if [ ! -z ${DB_BACKUP_PATH} ]; then
+      cd ${DB_BACKUP_PATH}
+      if [ ! -z ${DBDELDATE} ] && [ -d ${DBDELDATE} ]; then
+            rm -rf ${DBDELDATE}
+      fi
+fi
+
+### End of script ####
